a1db0fce4b63b1aba07b10d2f3be460fcdf21859
rijksuitgaven <contact@rijksuitgaven.nl>
2026-02-24 18:32:18 +0100

    Revert "Feat: UX-042 In-app release banner (staging only)"
    This reverts commit 8bd72c94ee13185c6010a1a979404719a2864dbc.

--- app/src/components/app-shell/app-shell.tsx
+++ app/src/components/app-shell/app-shell.tsx
@@ -4,7 +4,6 @@ import { useAuth } from '@/hooks/use-auth'
 import { Header } from '@/components/header'
 import { PublicHeader } from '@/components/homepage'
 import { SubscriptionBanner } from '@/components/subscription-banner'
-import { ReleaseBanner } from '@/components/release-banner'
 import { FeedbackButton } from '@/components/feedback'
 import { MobileBanner } from '@/components/mobile-banner'
 
@@ -22,7 +21,6 @@ export function AppShell({ children }: { children: React.ReactNode }) {
       {isLoggedIn ? (
         <>
           <Header />
-          <ReleaseBanner />
           <SubscriptionBanner />
         </>
       ) : (
